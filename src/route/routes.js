43ac0427d272d7c13df8e248a93484738fe2c146
haseebismail703 <haseebismail703@gmail.com>
2025-01-07 23:05:16 +0500

    adminProfile

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import multer  from "multer";
-import { signupUser, signinUser, adminLogin } from '../controler/authController.js'
+import { signupUser, signinUser, adminLogin,userProfile,profileUpdate } from '../controler/authController.js'
 import { allApRejRevTask, createTask, getallproofbyId, getTaskbyId, statusUpdate, UpdateTaskProf,addFund } from "../controler/advController.js";
 import { pendingTask, getTask, approve_task, reject_task, getPayment, getUser,PaymentHistory,blockUser, paidWithdrow ,getTaskReport,getReportTask} from '../controler/adminControler.js'
-import { getPaymentHistory, getTaskuser,getTaskByuser,myWork,submitTask, userPayment,reportTask,userProfile,profileUpdate } from '../controler/userControler.js'
+import { getPaymentHistory, getTaskuser,getTaskByuser,myWork,submitTask, userPayment,reportTask } from '../controler/userControler.js'
 const router = express.Router();
 const storage = multer.diskStorage({});
 
@@ -15,6 +15,8 @@ const upload = multer({
 router.post('/signinUser', signinUser);
 router.post('/signupUser', signupUser);
 router.post('/adminLogin', adminLogin);
+router.get('/userProfile/:id',userProfile);
+router.put('/profileUpdate/:id',upload.array('file', 1),profileUpdate);
 // advertiser routes 
 router.post('/createTask', createTask);
 router.get('/getTaskbyId/:id', getTaskbyId);
@@ -43,6 +45,5 @@ router.get('/myWork/:userId',myWork);
 router.post('/payment',userPayment);
 router.get('/getPaymentHistory/:id',getPaymentHistory);
 router.post('/reportTask',reportTask);
-router.get('/userProfile/:id',userProfile);
-router.put('/profileUpdate/:id',upload.array('file', 1),profileUpdate);
+
 export default router;
